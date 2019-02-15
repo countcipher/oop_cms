0169a8248048857d9a8fcdd2f47f9a3b201aa873
Chris <chrisharlow366@yahoo.com>
2019-02-15 15:41:18 -0500

    New commit

---
+++ cms/admin/includes/database.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("config.php");
+
+class Database{
+    
+    private $connection;
+    
+    public function open_db_connection(){
+        
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
+        if(mysqli_connect_errno()){
+            
+            die("Database connection failure ".mysqli_error());
+            
+        }
+        
+    }
+      
+    
+}// end of Database class
+
+$database = new Database();
+
+$database->open_db_connection();
+
+
+?>
