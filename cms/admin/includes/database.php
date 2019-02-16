361ff4a5aed76a20c31037c41f79f78b261b005a
Chris <chrisharlow366@yahoo.com>
2019-02-16 00:46:56 -0500

    Updates

--- cms/admin/includes/database.php
+++ cms/admin/includes/database.php
@@ -6,6 +6,12 @@ class Database{
     
     public $connection;
     
+    function __construct(){
+        
+        $this->open_db_connection();
+        
+    }//End of __construct
+    
     public function open_db_connection(){
         
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -16,14 +22,36 @@ class Database{
             
         }
         
+    }// end of open_db_connection
+    
+    public function query($sql){
+        
+        $result = mysqli_query($this->connection, $sql);
+        
+        return $result;
+        
+    }//end of query
+    
+    private function confirm_query($result){
+        
+        if(!$result){
+            die("Query failed");
+        }
+        
+    }//end of confirm_query
+    
+    public function escape_string($string){
+        
+        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        
+        return $escaped_string;
+        
     }
       
     
 }// end of Database class
 
 $database = new Database();
-
-$database->open_db_connection();
 
 
 ?>
