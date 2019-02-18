11054cdf80904e7a6ef81bb3d371b9f8e2eed304
Chris <chrisharlow366@yahoo.com>
2019-02-18 00:06:12 -0500

    update

--- cms/admin/includes/user.php
+++ cms/admin/includes/user.php
@@ -4,20 +4,27 @@ class User{
     
     
     static function find_all_users(){
-        global $database;
         
-        $result_set = $database->query("SELECT * FROM users");
-        
-        return $result_set;
+        return self::find_this_query("SELECT * FROM users");
         
     }
     
     static function find_user_by_id($user_id){
         global $database;
         
-        $result_set = $database->query("SELECT * FROM users WHERE id = $user_id");
+        $result_set = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
         $found_user = mysqli_fetch_assoc($result_set);
         return $found_user;
+        
+    }
+    
+    public static function find_this_query($sql){
+        global $database;
+        
+        $result_set = $database->query($sql);
+        
+        return $result_set;
+        
         
     }
     
